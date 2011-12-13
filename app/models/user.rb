648394f4f1c4d9ded502d9ada3a2be6fc00cacac
lionel8000 <8836272@qq.com>
2011-12-13 21:45:10 +0800

    User model with password and user show page next will be the sign up page,so stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,44 @@
 #  created_at :datetime
 #  updated_at :datetime
 #
-
+require 'digest'
 class User < ActiveRecord::Base
+	attr_accessor :password
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	attr_accessible :name,:email
+	attr_accessible :name,:email,:password,:password_confirmation,:encrypted_password
 	validates :name,:presence =>true,:length=>{:maximum =>10}
 	validates :email,:presence =>true,:format=>{:with=>email_regex},
                   :uniqueness => {:case_sensitive => false}
+	validates :password,:confirmation =>true,
+		  :length => {:within=>6..40}
+  	before_save :encrypt_password
+
+        def has_password?(submitted_password)
+ 		encrypted_password == encrypt(submitted_password)
+        end
+        
+ 	def self.authenticate(email,matchpass)
+            user = find_by_email(email)
+            return nil if user.nil?
+            return user if user.has_password?(matchpass)
+	end
+
+   	private
+             def encrypt_password
+                 self.salt = make_salt unless has_password?(password)
+                 self.encrypted_password = encrypt(password)
+ 	     end
+             
+             def  encrypt(string)
+                  secure_hash("#{salt}--#{string}")
+             end
+             
+             def make_salt
+                 secure_hash("#{Time.now.utc}--#{password}")
+             end 
+             
+             def secure_hash(string)
+                  Digest::SHA2.hexdigest(string)
+             end                   
 	
 end
